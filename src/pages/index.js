1237a20d80b5b39732ec9e5dfe41fd4d861a312c
inksharedsolutions <webdev.inkshared@gmail.com>
2021-03-10 05:33:39 +0800

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,41 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
-
+import React from "react"
+import {Link} from "gatsby"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Main from '../components/main'
+import MainBottom  from '../components/main-bottom'
+import Newsletter  from '../components/newsletter'
+import Banner from "../components/banner"
+import { Helmet } from "react-helmet"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
-  </Layout>
-)
+const IndexPage = () => {
+	
+
+	const bannerText = (
+		<>
+			<span className="banr-tagline-fx">Hampton Scott Tonk</span>
+			<h1 className="banr-header-fx">
+				<span className="banr-spn-2">It's God's time. It's Kingdom's time. It's the fullness of time. It's</span> 
+				<span className="banr-spn-highlight">God's Oppotunity!</span>
+				
+				<Link
+					key=""
+					className="btn-banner-intro" 
+					to="/books#you-can-too">Read More
+				</Link>
+			</h1>
+		</>
+	)
+
+	return (
+	  	<Layout>
+			  <Helmet title="Home | Hampton Scott Tonk"/>
+	  		 <Banner bannerContext={bannerText} />
+			 <Main/>
+			 <MainBottom />
+			 <Newsletter />
+	  	</Layout>
+  	)
+
+}
 
 export default IndexPage
